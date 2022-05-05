fbc57ff272bf69d0e62479d6ff5c478f9df11bec
Jesse Kovash <jessekovash@gmail.com>
2022-05-05 16:10:15 -0600

    programmatic piano complete. Plays with both mouse and keyboard. Keys and sounds generated purely with JS.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Piano
+# Programmatic Piano
+
+## How To Play
+
+### Open index.html in your browsewr
+
+### Click on a key with your mouse. It plays as long as your mouse is held down and stops on release.
+
+### You can also play with your keyboard. The two bottom rows of the keyboard (starts with 'a' and 'z') are the keys to use.
+
+
+#### About: Pure JS to create the keys and the sounds. Audio context oscillator node used to play specific frequencies.
--- index.js
+++ index.js
@@ -2,28 +2,46 @@ window.addEventListener('DOMContentLoaded', (event) => {
   let keyboard = document.querySelector('.keyboard');
   let keys = [];
   const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  let notes = document.querySelectorAll('.key');
   let oscillator;
   let count = 25;
+  let keyCodes = [90, 83, 88, 68, 67, 70, 86, 71, 66, 72, 78, 74, 77, 75, 188, 76, 190, 186, 191];
 
-  const playSound = function(event) {
-    console.log('inplaysound')
+  const playSound = function(event, keyCode) {
+    let key;
+    if (keyCode) {
+      key = keys[keyCodes.indexOf(keyCode)] || null
+    } else  {
+      key = event.target;
+    }
 
-    oscillator = audioCtx.createOscillator();
-    oscillator.type = 'square';
-    oscillator.frequency.setValueAtTime(event.target.id, audioCtx.currentTime);
-    oscillator.connect(audioCtx.destination);
-    oscillator.start();
-    setTimeout(()=> {
-      console.log('insettimeout')
-      stopSound(event)
-    }, 700)
+    if (key) {
+      key.classList.add('active');
+      oscillator = audioCtx.createOscillator();
+      oscillator.type = 'square';
+      oscillator.frequency.setValueAtTime(key.id, audioCtx.currentTime);
+      oscillator.connect(audioCtx.destination);
+      oscillator.start();
+      setTimeout(()=> {
+        stopSound(event)
+      }, 700)
+    }
   }
 
-  const stopSound = function(event) {
-    oscillator.stop()
+  const stopSound = function(event, keyCode) {
+    let key;
+    if (keyCode) {
+      key = keys[keyCodes.indexOf(keyCode)] || null;
+    } else  {
+      key = event.target;
+    }
+    if (key) {
+      key.classList.remove('active');
+      oscillator.stop()
+    }
   }
 
-  for (var i=0; i < 37; i++) {
+  for (var i=0; i < 19; i++) {
     let key = document.createElement('div');
     if (i % 2 === 0) {
       key.classList.add('white')
@@ -34,9 +52,13 @@ window.addEventListener('DOMContentLoaded', (event) => {
     key.addEventListener('mouseup', stopSound)
     key.classList.add('key')
     key.setAttribute('id', count);
+    key.setAttribute('value', keyCodes[i]);
     count+=25
     keys.push(key)
   }
   keyboard.append(...keys)
 
+  document.addEventListener('keydown', (event)=>(playSound(event, event.keyCode)))
+
+  document.addEventListener('keyup', (event)=>(stopSound(event, event.keyCode)))
 });
